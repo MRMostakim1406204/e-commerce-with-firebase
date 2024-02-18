b1d4273f52e288a364b79c598393f8ec41bd7d9c
MRMostakim1406204 <mdmrmostakim1406204@gmail.com>
2024-02-18 16:02:46 +0600

    E-Commerce With Firebase

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:ecommerce_project/design.dart/productDetails.dart';
+import 'package:ecommerce_project/design.dart/splash.dart';
+import 'package:ecommerce_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: (context ,index) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter E-Commerce',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
